685619df951e9154c5e4e46850ed174a4f48e030
Lukas Bindreiter <lukas.bindreiter@tilebox.com>
2025-10-31 09:56:06 +0100

    Make network configurable

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This library provides the following [Pulumi](https://www.pulumi.com/) resources:
 - A `LocalBuildTrigger` ressource that runs a local command to build a Docker image on code changes and pushes it to a Google Artifact Registry repository
 - A `Secret` resource to manage GCP secrets
 - A `AutoScalingGCPCluster` resource to manage the auto-scaling cluster
-- A `TileboxNetwork` resource to manage the network with Private Google Access (PGA) enabled and a router for outbound internet access
+- A `GCPNetwork` resource to manage networking within the Cluster, optionally enabling Private Google Access (PGA) and a router for outbound internet access
 
 ## Dev setup
 
--- tilebox_iac/network.py
+++ tilebox_iac/network.py
@@ -2,21 +2,30 @@ from pulumi import ComponentResource, ResourceOptions
 from pulumi_gcp.compute import Network, Router, RouterNat, Subnetwork
 
 
-class TileboxNetwork(ComponentResource):
+class GCPNetwork(ComponentResource):
     def __init__(
         self,
         name: str,
         gcp_region: str,
+        enable_private_google_access: bool = True,
+        enable_internet_access: bool = True,
         opts: ResourceOptions | None = None,
     ) -> None:
-        """A network with Private Google Access (PGA) enabled and a router for outbound internet access.
+        """A network with optional Private Google Access (PGA) and an optional router for outbound internet access.
+
+        If
+        Private Google Access (PGA) allows VMs to access Google APIs and services using an internal IP address.
+        That way, Cloud services or GCS buckets can be accessed without incurring egress charges.
+
+        If `enable_internet_access` is set to `True`, a router and RouterNAT are created to allow VMs to
+        access the internet (outbound).
 
         Args:
             name: Name of the network.
             gcp_region: The GCP region to deploy the network in.
             opts: Pulumi resource options.
         """
-        super().__init__("tilebox:TileboxNetwork", name, opts=opts)
+        super().__init__("tilebox:GCPNetwork", name, opts=opts)
 
         self.network = Network(
             f"{name}-network",
@@ -24,32 +33,40 @@ class TileboxNetwork(ComponentResource):
             auto_create_subnetworks=False,
             opts=ResourceOptions(parent=self),
         )
-        self.pga_subnet = Subnetwork(
-            f"{name}-pga-subnet",
-            name=f"{name}-pga-subnet",
+        self.subnet = Subnetwork(
+            f"{name}-subnet",
+            name=f"{name}-subnet",
             ip_cidr_range="10.10.0.0/24",
             network=self.network.self_link,
             region=gcp_region,
-            private_ip_google_access=True,  # Private Google Access (PGA) enabled
+            private_ip_google_access=enable_private_google_access,
             opts=ResourceOptions(depends_on=[self.network], parent=self),
         )
 
-        # Router and RouterNAT allow VMs to access the internet (outbound)
-        self.router = Router(
-            f"{name}-router",
-            name=f"{name}-router",
-            network=self.network.self_link,
-            region=gcp_region,
-            opts=ResourceOptions(depends_on=[self.network], parent=self),
-        )
-        self.router_nat = RouterNat(
-            f"{name}-nat",
-            name=f"{name}-nat",
-            router=self.router.name,
-            region=gcp_region,
-            source_subnetwork_ip_ranges_to_nat="ALL_SUBNETWORKS_ALL_IP_RANGES",
-            nat_ip_allocate_option="AUTO_ONLY",
-            opts=ResourceOptions(depends_on=[self.router], parent=self),
-        )
+        if enable_internet_access:
+            # Router and RouterNAT allow VMs to access the internet (outbound)
+            self.router = Router(
+                f"{name}-router",
+                name=f"{name}-router",
+                network=self.network.self_link,
+                region=gcp_region,
+                opts=ResourceOptions(depends_on=[self.network], parent=self),
+            )
+            self.router_nat = RouterNat(
+                f"{name}-nat",
+                name=f"{name}-nat",
+                router=self.router.name,
+                region=gcp_region,
+                source_subnetwork_ip_ranges_to_nat="LIST_OF_SUBNETWORKS",
+                subnetworks=[
+                    {
+                        "name": self.subnet.id,
+                        "source_ip_ranges_to_nats": ["ALL_IP_RANGES"],
+                    }
+                ],
+                nat_ip_allocate_option="AUTO_ONLY",
+                opts=ResourceOptions(depends_on=[self.router], parent=self),
+            )
 
-        self.register_outputs({})
+        self.id = self.network.id
+        self.register_outputs({"id": self.id})
