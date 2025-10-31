8c7ce43c9e0566d723549eb86813bc200d808133
Lukas Bindreiter <lukas.bindreiter@tilebox.com>
2025-10-31 09:57:27 +0100

    Fix import

--- tilebox_iac/__init__.py
+++ tilebox_iac/__init__.py
@@ -1,6 +1,6 @@
 from tilebox_iac.auto_scaling_cluster import AutoScalingGCPCluster
 from tilebox_iac.image_builder import LocalBuildTrigger
-from tilebox_iac.network import TileboxNetwork
+from tilebox_iac.network import GCPNetwork
 from tilebox_iac.secrets import Secret
 
-__all__ = ["AutoScalingGCPCluster", "LocalBuildTrigger", "Secret", "TileboxNetwork"]
+__all__ = ["AutoScalingGCPCluster", "GCPNetwork", "LocalBuildTrigger", "Secret"]
